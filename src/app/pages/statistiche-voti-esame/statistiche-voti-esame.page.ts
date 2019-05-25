5ea1e7308156cfe22f5b2a19e5a752c730613a22
phabreetion <fabrizioformichelli3@outlook.it>
2019-05-25 17:40:54 +0200

    grafico a torta cambiato e sistemato

--- src/app/pages/statistiche-voti-esame/statistiche-voti-esame.page.ts
+++ src/app/pages/statistiche-voti-esame/statistiche-voti-esame.page.ts
@@ -33,22 +33,17 @@ export class StatisticheVotiEsamePage implements OnInit {
     };
     public lineChartLegend: boolean = true;
     public lineChartType: string = 'line';
-    public barChartLabels: string[] = ['2012', '2013', '2014', '20015', '2016', '2017', '2018'];
+    public barChartLabels: string[] = ['18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '30L'];
     public barChartType: string = 'bar';
     public barChartLegend: boolean = true;
 
     public barChartData: any[] = [
-        {data: [20, 24, 22, 30, 27, 19, 26], label: '1°Anno'},
-        {data: [21, 24, 28, 20, 27, 30, 19], label: '2°Anno'},
-        {data: [23, 25, 29, 23, 30, 22, 26], label: '3°Anno'},
-        {data: [21, 23, 26, 24, 28, 30, 22], label: 'Fuori corso'}
+        {data: [20, 24, 22, 30, 27, 19, 26, 11, 25, 11, 28, 5, 6], label: 'Distribzione voti'},
     ];
 
+
     public lineChartData: Array<any> = [
-        {data: [20, 24, 22, 30, 27, 19, 26], label: '1°Anno'},
-        {data: [21, 24, 28, 20, 27, 30, 19], label: '2°Anno'},
-        {data: [23, 25, 29, 23, 30, 22, 26], label: '3°Anno'},
-        {data: [21, 23, 26, 24, 28, 30, 22], label: 'Fuori corso'}
+        {data: [20, 24, 22, 30, 27, 19, 26], label: 'Media voti'},
     ];
     public lineChartLabels: Array<any> = ['2012', '2013', '2014', '2015', '2016', '2017', '2018'];
     public lineChartOptions: any = {
